cf2ef4733afb13e0234e4eaf4e26ce5a0984140f
vamsi1999 <vamsi.k6669@gmail.com>
2021-06-24 23:33:16 +0530

    initial commit

---
+++ add prog.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,res;
+    printf("enter integers to add :");
+    scanf("%d%d",&a,&b);
+    res=a+b;
+    printf("addition is : %d",res);
+    return 0;
+
+}
